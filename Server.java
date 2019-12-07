d909f00e52d185541d9baf8059d426a1da2bff0d
LuisNegrao <luismarcosnegrao@gmail.com>
2019-12-07 19:09:41 +0000

    server 1.0

--- Server.java
+++ Server.java
@@ -1,57 +1,214 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-/**
- * Server
- */
+class Client {
+
+    private SocketChannel sc;
+    private String nick;
+    private String status;
+    private String room;
+
+    public Client(SocketChannel sChannel) {
+
+        this.sc = sChannel;
+        this.nick = null;
+        this.room = "lobby";
+        this.status = "init";
+    }
+
+
+    public SocketChannel getSc() {
+        return this.sc;
+    }
+
+    public void setSc(SocketChannel sc) {
+        this.sc = sc;
+    }
+
+    public String getNick() {
+        return this.nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+}
+
+class Room {
+
+    private String name;
+    private LinkedList<Client> clients;
+
+    public Room (String name) {
+        this.name = name;
+        clients = new LinkedList<>();
+    }
+
+    public void addClients(Client client) {
+        client.setRoom(this.name);
+        clients.add(client);
+    }
+
+    public void removeClient(Client client) {
+        client.setRoom(null);
+        clients.remove(client);
+        //TODO remove room if size == 0
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public LinkedList<Client> getClients() {
+        return this.clients;
+    }
+
+}
+
+
+
+
 public class Server {
 
-    Socket socket = null;
-    ServerSocket serverSocket = null;
-    DataInputStream in = null;
+    private static ByteBuffer buffer = ByteBuffer.allocate(16384);
 
-    public Server(int port) {
+    private static Charset charset = Charset.forName("UTF8");
+    private static CharsetDecoder decoder = charset.newDecoder();
+    private static CharsetEncoder encoder = charset.newEncoder();
+    private static String message = "";
+    static LinkedList<Client> cli1ents = new LinkedList<>();
+    private static LinkedList<Room> rooms = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+        int port = args[0];
 
         try {
+            
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
 
-            serverSocket = new ServerSocket(port);
-            System.out.println("Server is Up");
+            ServerSocket serverSocket = serverSocketChannel.socket();
+            InetSocketAddress iSocketAddress = new InetSocketAddress(port);
+            socket.bind(iSocketAdress);
 
-            socket = serverSocket.accept();
-            System.out.println("New Client Joined");
+            Selector selector = Selector.open();
 
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println( "Listening on port "+port );
+            
+            while (true) {
 
-            String msg = "";
+                int num = selector.select();
 
-            while (!msg.equals("Quit")) {
+                if (num == 0) 
+                    continue;
                 
-                try {
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = keys.iterator();
 
-                    msg = in.readUTF();
-                    System.out.println(msg);
+                while (iterator.hasNext()) {
 
-                } catch (Exception e) {
-                    //TODO: handle exception
-                    e.printStackTrace(); 
+                    SelectionKey key = iterator.next();
+
+                    if ((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
+                        
+                        Socket socket = serverSocket.accept();
+
+                        SocketChannel sc = socket.getChannel();
+                        sc.configureBlocking(false);
+
+                        sc.register(selector, SelectionKey.OP_READ);
+
+                    } else if ((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
+
+                        SocketChannel sc = null;
+
+                        try {
+                            
+                            sc = (SocketChannel)key.channel();
+                            boolean ok = processInput( sc );
+
+                            if(!ok) {
+                                key.cancel();
+
+                                Socket s = null;
+                                try {
+                                    s = sc.socket();
+                                    s.close();
+                                } catch (Exception e) {
+                                    System.err.println("Error closing socket" + s);
+                                }
+                            }
+
+                        } catch (IOException e) {
+
+                            key.cancel();
+
+                            try {
+                                sc.close();
+                            } catch (IOException e2) {
+                                System.out.println(e2);
+                            }
+                            System.out.println( "Closed "+sc );
+                        }
+
+                    }
+
                 }
+
+                keys.clear();
 
             }
 
-            socket.close();
-            in.close();
-
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println(e);
         }
 
     }
 
-    public static void main(String[] args) {
-
-        int port = Integer.parseInt(args[0]);
-        Server server = new Server(port);
+    static private boolean processInput( SocketChannel sc ) throws IOException {
+        // Read the message to the buffer
+        buffer.clear();
+        sc.read( buffer );
+        buffer.flip();
+    
+        // If no data, close the connection
+        if (buffer.limit()==0) {
+          return false;
+        }
+    
+        // Decode and print the message to stdout
+        String message2 = decoder.decode(buffer).toString();
+        System.out.print( message2 );
+    
+        return true;
     }
     
 }
